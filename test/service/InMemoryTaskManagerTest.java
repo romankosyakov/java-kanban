ad3d2e614b94dbc9afcca6cc68451744d01dc73c
Roman Kosyakov <kosyakovroman@yandex.ru>
2025-07-30 21:21:59 +0300

    5-ФЗ. 2я итерация

--- test/service/InMemoryTaskManagerTest.java
+++ test/service/InMemoryTaskManagerTest.java
@@ -61,6 +61,7 @@ public class InMemoryTaskManagerTest {
         assertNotEquals(epic1, sub1);
     }
 
+    //deleting
     @Test
     void deleteAllClearsEverything() {
         Task t = new Task("T", "D", Status.NEW);
@@ -80,5 +81,69 @@ public class InMemoryTaskManagerTest {
 
         m.deleteAllTasks();
         assertTrue(m.getTasks().isEmpty());
+    }
+
+    //getting
+    @Test
+    void canGetEverything() {
+        Task t = new Task("T", "D", Status.NEW);
+        m.addNewTask(t);
+        Epic e = new Epic("E", "D");
+        m.addNewEpic(e);
+        Subtask s = new Subtask("S", "D", Status.NEW, e.getId());
+        m.addNewSubtask(s);
+
+        assertEquals(t, m.getTaskById(t.getId()));
+        assertEquals(e, m.getEpicById(e.getId()));
+        assertEquals(s, m.getSubtaskById(s.getId()));
+    }
+
+    //updating
+    @Test
+    void updateTaskTest() {
+        Task t1 = new Task("T", "D", Status.NEW);
+        m.addNewTask(t1);
+        t1.setStatus(Status.DONE);
+        m.updateTask(t1);
+        Task t2 = new Task("T", "D", Status.DONE);
+        t2.setId(t1.getId());
+        assertEquals(t1, t2);
+    }
+
+    @Test
+    void updateEpicTest() {
+        Epic e1 = new Epic("E", "D");
+        m.addNewEpic(e1);
+        e1.setStatus(Status.DONE);
+        m.updateEpic(e1);
+        Epic e2 = new Epic("E", "D");
+        e2.setId(e1.getId());
+        assertEquals(e1, e2);
+    }
+
+    @Test
+    void updateSubtaskTest() {
+        Epic e1 = new Epic("E", "D");
+        m.addNewEpic(e1);
+        int epicId = e1.getId();
+        Subtask s1 = new Subtask("T", "D", Status.NEW, epicId);
+        m.addNewSubtask(s1);
+        s1.setStatus(Status.DONE);
+        m.updateSubtask(s1);
+        Subtask s2 = new Subtask("T", "D", Status.DONE, epicId);
+        s2.setId(s1.getId());
+        assertEquals(s1, s2);
+    }
+
+    @Test
+    void updateEpicStatusTest() {
+        Epic e1 = new Epic("E", "D");
+        m.addNewEpic(e1);
+        assertEquals(Status.IN_PROGRESS, e1.getStatus());
+        Subtask s1 = new Subtask("S1", "Desc", Status.NEW, e1.getId());
+        m.addNewSubtask(s1);
+        Subtask s2 = new Subtask("S2", "Desc", Status.DONE, e1.getId());
+        m.addNewSubtask(s2);
+        assertEquals(Status.IN_PROGRESS, e1.getStatus());
     }
 }
