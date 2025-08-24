9b73be64419890c6ffa023c08eecffb7b06ed74f
Roman Kosyakov <kosyakovroman@yandex.ru>
2025-08-24 22:54:16 +0300

    6-ФЗ. 1я итерация

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -2,24 +2,85 @@ package service;
 
 import model.Task;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final List<Task> memoryList = new ArrayList<>();
+
+    private static class Node {
+
+        private final Task task;
+        private Node next;
+        private Node prev;
+
+        private Node(Node prev, Task task, Node next) {
+            this.task = task;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    private final Map<Integer, Node> memoryMap = new HashMap<>();
+    private Node head;
+    private Node tail;
+
+    private void linkLast(int taskId, Node node) {
+        node.prev = tail;
+        memoryMap.put(taskId, node);
+        tail = node;
+        if (head == null) {
+            head = node;
+        } else {
+            tail.prev.next = node;
+        }
+    }
+
 
     @Override
     public void addInHistory(Task task) {
         if (task != null) {
-            if (memoryList.size() > 10) {
-                memoryList.removeFirst();
+            int taskId = task.getId();
+            Node newNode = new Node(null, task, null);
+
+            if (!memoryMap.containsKey(taskId)) {
+                linkLast(taskId, newNode);
+            } else {
+                removeNode(taskId);
+                linkLast(taskId, newNode);
             }
-            memoryList.add(task);
+
         }
     }
 
     @Override
     public List<Task> getHistory() {
-        return new ArrayList<>(memoryList);
+        List<Task> history = new ArrayList<>();
+        Node current = head;
+        while (current != null) {
+            history.add(current.task);
+            current = current.next;
+        }
+        return history;
+    }
+
+    @Override
+    public void removeNode(int id) {
+        Node nodeToRemove = memoryMap.get(id);
+        if (nodeToRemove != null) {
+            Node prevNode = nodeToRemove.prev;
+            Node nextNode = nodeToRemove.next;
+
+            if (prevNode != null) {
+                prevNode.next = nextNode;
+            } else {
+                head = nextNode;
+            }
+
+            if (nextNode != null) {
+                nextNode.prev = prevNode;
+            } else {
+                tail = prevNode;
+            }
+            memoryMap.remove(id);
+        }
     }
 }
--- test/service/InMemoryHistoryManagerTest.java
+++ test/service/InMemoryHistoryManagerTest.java
@@ -4,7 +4,9 @@ import model.Task;
 import model.Status;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class InMemoryHistoryManagerTest {
@@ -12,18 +14,24 @@ public class InMemoryHistoryManagerTest {
     private HistoryManager historyManager;
     private Task task1;
     private Task task2;
+    private Task task3;
+    private Task task4;
 
     @BeforeEach
     void setup() {
         historyManager = new InMemoryHistoryManager();
         task1 = new Task("Name1", "Desc1", Status.NEW);
         task2 = new Task("Name2", "Desc2", Status.NEW);
+        task3 = new Task("Name3", "Desc3", Status.NEW);
+        task4 = new Task("Name4", "Desc4", Status.NEW);
+        task1.setId(1);
+        task2.setId(2);
+        task3.setId(3);
+        task4.setId(4);
     }
 
     @Test
     void shouldAddTasksToHistory() {
-        task1.setId(100);
-        task2.setId(32);
         historyManager.addInHistory(task1);
         historyManager.addInHistory(task2);
         List<Task> hist = historyManager.getHistory();
@@ -33,14 +41,16 @@ public class InMemoryHistoryManagerTest {
     }
 
     @Test
-    void shouldNotExceedMaxHistorySize() {
-        for (int i = 1; i <= 12; i++) {
-            Task t = new Task("Task" + i, "D", Status.NEW);
-            t.setId(i);
-            historyManager.addInHistory(t);
-        }
-        List<Task> history = historyManager.getHistory();
-        assertEquals(11, history.size());
-        assertEquals(2, history.getFirst().getId());
+    void shouldDeletePreviousSameTaskInHistory() { // так же проверяет работу linkLast и removeNode
+        historyManager.addInHistory(task1);
+        historyManager.addInHistory(task2);
+        historyManager.addInHistory(task3);
+        historyManager.addInHistory(task2);
+        List<Task> hist = historyManager.getHistory();
+        assertEquals(3, hist.size());
+        assertEquals("Name1", hist.get(0).getName());
+        assertEquals("Name3", hist.get(1).getName());
+        assertEquals("Name2", hist.get(2).getName());
     }
+
 }
