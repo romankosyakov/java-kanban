5197d465967ef20e7763446c70267d24b7ed14fa
Roman Kosyakov <kosyakovroman@yandex.ru>
2025-09-10 19:45:44 +0300

    7-ФЗ. 1я итерация

--- src/exceptions/ManagerSaveException.java
+++ src/exceptions/ManagerSaveException.java
@@ -5,6 +5,7 @@ public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(String message, Throwable cause) {
         super(message, cause);
     }
+
     public ManagerSaveException(String message) {
         super(message);
     }
